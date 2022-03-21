26766df94a38eb0c2bdc999c4ce5ab861eff1213
warat11_6204062616111 <y-title@hotmail.com>
2022-03-21 20:22:30 +0700

    [UPDATE]Newton

--- src/views/Chapter1/Mainchapter1.tsx
+++ src/views/Chapter1/Mainchapter1.tsx
@@ -4,10 +4,12 @@ import { addStyles, EditableMathField,StaticMathField} from "react-mathquill"
 
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import axios from "axios";
-import { parse, evaluate,serialize,LatexSyntax,ComputeEngine  } from '@cortex-js/compute-engine';
+import { parse,serialize,LatexSyntax,ComputeEngine  } from '@cortex-js/compute-engine';
 import {MathfieldElement} from "mathlive";
 import { MathfieldComponent } from "react-mathlive";
-import {string } from "mathjs";
+import * as mathjs from "mathjs";
+import { parseTex, evaluateTex } from 'tex-math-parser' // ES6 module
+import CalProblem from "./CalProblem";
 
 
 
@@ -37,7 +39,6 @@ const Mainchapter1:React.FC<test> =(props)=>{
     const [atex, setatex] = React.useState("f(x)=\\log _10 x");
 
 
-
     // const expr = parse('\\frac{\\sqrt{5}}{3}')
     // const atex = serialize(expr);
     // console.log(atex)
@@ -51,69 +52,52 @@ const Mainchapter1:React.FC<test> =(props)=>{
 // console.log(expr);
 // const atex = latexSyntax.serialize(expr);
 // console.log(atex);
+
+
 const ce = new ComputeEngine();
-console.log("latex is",latex)
-let c:string = "8";
-let d:any = parseFloat(c)
-console.log(d);
-let b :string = latex.replace(/(\d+)(x)/g, '$1*x').replace(/(\d+)(e)/g, '$1*e').replace(/x/g,d)
-console.log("latex convert is",b)
-console.log("cal is",ce.N(parse(b)))
+// console.log("latex is",latex)
+// let c:string = "8";
+// let d:any = parseFloat(c)
+// let e:string = '2^{\\frac{1}{2}}'
+// console.log(d);
+// let b :string = e.replace(/(\d+)(x)/g, '$1*x').replace(/(\d+)(e)/g, '$1*e').replace(/x/g,d)
+// console.log("latex convert is",b)
+// console.log("cal is",ce.N(parse(b)))
 
 
 
-    // const texStr = String.raw`x^{\frac{x}{3}}`;
-    // const answer = evaluateTex(texStr, {x: 3});
-    // console.log(answer.evaluated); // 1
 
-    useEffect(()=>{
-        axios.get("http://localhost:6060/posts")
-        .then(response => {
-            
-            switch(props.name){
-                case "Bisection":
-                 setLatex(response.data.keepquestion.Bisection[0].eq)
-                 settest({left:response.data.keepquestion.Bisection[0].left,right:response.data.keepquestion.Bisection[0].right,begin:'0'})
-                 break
-                 case "FalsePosition":
-                 setLatex(response.data.keepquestion.FalsePosition[0].eq)
-                 settest({left:response.data.keepquestion.FalsePosition[0].left,right:response.data.keepquestion.FalsePosition[0].right,begin:'0'})
-                 break
-                 case "OnePointInteration":
-                 setLatex(response.data.keepquestion.OnePointInteration[0].eq)
-                 settest({left:'0',right:'0',begin:response.data.keepquestion.OnePointInteration[0].xbegin})
-                 break
-            }
-            // do something about response
-        })
-        
-        .catch(err => {
-            console.error(err)
-        })
-    },[])
+
+// const texStr = String.raw`2^{\frac{x}{4}}`;
+// const answer = evaluateTex(texStr, {x: 2, y: 1});
+// console.log(answer.evaluated); // 1
+
+
+
     
-  
-    const handleSubmit = async (e:any)=> {
-        alert('ขอบเขตซ้าย: '+test.left+' '+'ขอบเขตขวา: '+test.right);
-        
+    const problem = (left:string,right:string,begin:string,eq:string) =>{
         const fx = (x:number) =>{
             let X:number = parseFloat(x.toFixed(9))
             console.log('X',X)
-            let pow:string = latex.replace(/(\d+)(x)/g, '$1*x').replace(/(\d+)(e)/g, '$1*e').replace(/x/g,X.toString())
-            console.log('pow is',pow)
-            let a :any = ce.N(parse(pow.replace(/x/g,X.toString())))
-            console.log('ans is',a)
+            let pow:string = eq.replace(/([-+]?[0-9]*\.?[0-9]+)(x)/g, '$1*x').replace(/([-+]?[0-9]*\.?[0-9]+)(e)/g, '$1*e').replace(/x/g,X.toString()).replace(/\\frac{([-+]?[0-9]*\.?[0-9]+)}{([-+]?[0-9]*\.?[0-9]+)}/,'$1/$2').replace(/\\sqrt{([-+]?[0-9]*\.?[0-9]+)}/,'sqrt($1)').replace(/{/g,'(').replace(/}/g,')')
+            console.log("samagan ku is ",pow)
+            // console.log('pow is',pow)
+            // let a :any = ce.N(parse(pow.replace(/x/g,X.toString())))
+            // console.log('ans is',a)
+            let eee : any = mathjs.parse(pow)
+            let a : any = eee.evaluate()
             return a
         }
         const Bisection = () => {
-            let Xl : number =  parseFloat(test.left)
-            let Xr : number = parseFloat(test.right)
+            let Xl : number =  parseFloat(left)
+            let Xr : number = parseFloat(right)
             console.log(Xl+Xr)
             let x_old : number = 0
             let Xm : number = (Xl+Xr)/2
             console.log("ssss="+Xl+Xr)
+            let error : number = Math.abs((Xm-x_old)/Xm)
 
-            while (Math.abs((Xm-x_old)/Xm) > 0.0000001){
+            while (error > 0.000001 && error != Infinity){
                 if(fx(Xm)*fx(Xr)>0){
                     x_old = Xr
                     Xr = Xm
@@ -126,6 +110,7 @@ console.log("cal is",ce.N(parse(b)))
                 }
                 Xm = (Xl+Xr)/2
                 console.log("Xm = "+Xm)
+                error = Math.abs((Xm-x_old)/Xm)
                 
 
                 const newCar = {
@@ -142,7 +127,7 @@ console.log("cal is",ce.N(parse(b)))
         }
         
         const FalsePosition = () => {
-            let Xl:number = parseFloat(test.left),Xr:number = parseFloat(test.right),Fxl:number = fx(Xl),Fxr:number = fx(Xr)
+            let Xl:number = parseFloat(left),Xr:number = parseFloat(right),Fxl:number = fx(Xl),Fxr:number = fx(Xr)
             let X1:number = ((Xl * Fxr) - (Xr * Fxl))/(Fxr-Fxl)
             let x_old:number = 0;
             while (Math.abs((X1-x_old)/X1) > 0.000001){
@@ -171,7 +156,7 @@ console.log("cal is",ce.N(parse(b)))
         }
         const Onepoint = () => {
             console.log("begin one point")
-            let x_old:number = 0
+            let x_old:number = parseFloat(begin)
             let X2:number = fx(x_old)
             let x_old_old:number  = 0
             let error:number = Math.abs((X2-x_old)/X2)
@@ -191,26 +176,28 @@ console.log("cal is",ce.N(parse(b)))
             setCars(datas)
             return X2
         }
-
-        const divfx = (x:number) => {   
-            let div = 5
-            return  div
-        }
         const fxnewton = (x:number) => {
-            let eqnewton = -fx(x)/divfx(x)
+            let div:number = mathjs.derivative(eq.replace(/([-+]?[0-9]*\.?[0-9]+)(x)/g, '$1*x').replace(/([-+]?[0-9]*\.?[0-9]+)(e)/g, '$1*e').replace(/\\frac{(.+)}{(.+)}/,'$1/$2').replace(/\\sqrt{([-+]?[0-9]*\.?[0-9]+)}/,'sqrt($1)').replace(/{/g,'(').replace(/}/g,')'),'x').evaluate({x:x})
+            let eqnewton:number = -fx(x)/div
             return eqnewton
         }
         const NewtonRaphson = () =>{
-            let xold = 2
+            let xold = parseFloat(begin)
             let eqnewton = fxnewton(xold)
             let xnew = xold + eqnewton
-            let error = Math.abs(xnew-xold)/xnew
+            let error = Math.abs((xnew-xold)/xnew)
             console.log(xnew)
-            while (error > 0.0000001 && error !== Infinity){
+            while (error > 0.000001 && error !== Infinity){
                 xold = xnew
                 xnew = xold + fxnewton(xold)
                 error = Math.abs(xnew-xold)/xnew
+                const newCar = {
+                    name: datas.length,
+                    Xm: xnew
+                  }
+                  datas.push(newCar)
             }
+            setCars(datas)
             return xnew
         }
 
@@ -229,14 +216,53 @@ console.log("cal is",ce.N(parse(b)))
                 break
         }
 
-        // let a = derivative(latex, 'x').toString()
-        // let b = eval(a)
+    }
+    const handleSubmit = async (e:any)=> {
+        alert('ขอบเขตซ้าย: '+test.left+' '+'ขอบเขตขวา: '+test.right);
+        console.log("handleeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee")
+        // let a = derivative('x^2+2x', 'x').toString()
+        console.log("latex submit",latex)
+        problem(test.left,test.right,test.begin,latex)
+
         e.preventDefault();
     }
 
     const inputsHandler = (e:any) => {
         settest({ ...test, [e.target.name]: e.target.value });
     };
+    useEffect(()=>{
+        axios.get("http://localhost:6060/posts")
+        .then(response => {
+            
+            switch(props.name){
+                case "Bisection":
+                 setLatex(response.data.keepquestion.Bisection[0].eq)
+                 settest({left:response.data.keepquestion.Bisection[0].left,right:response.data.keepquestion.Bisection[0].right,begin:'0'})
+                 problem(response.data.keepquestion.Bisection[0].left,response.data.keepquestion.Bisection[0].right,'0',response.data.keepquestion.Bisection[0].eq)
+                 break
+                 case "FalsePosition":
+                 setLatex(response.data.keepquestion.FalsePosition[0].eq)
+                 settest({left:response.data.keepquestion.FalsePosition[0].left,right:response.data.keepquestion.FalsePosition[0].right,begin:'0'})
+                 problem(response.data.keepquestion.FalsePosition[0].left,response.data.keepquestion.FalsePosition[0].right,'0',response.data.keepquestion.FalsePosition[0].eq)
+                 break
+                 case "OnePointInteration":
+                 setLatex(response.data.keepquestion.OnePointInteration[0].eq)
+                 settest({left:'0',right:'0',begin:response.data.keepquestion.OnePointInteration[0].xbegin})
+                 problem('0','0',response.data.keepquestion.OnePointInteration[0].xbegin,response.data.keepquestion.OnePointInteration[0].eq)
+                 break
+                 case "NewtonRaphson":
+                 setLatex(response.data.keepquestion.NewtonRaphson[0].eq)
+                 settest({left:'0',right:'0',begin:response.data.keepquestion.NewtonRaphson[0].xbegin})
+                 problem('0','0',response.data.keepquestion.NewtonRaphson[0].xbegin,response.data.keepquestion.NewtonRaphson[0].eq)
+                 break
+            }
+            // do something about response
+        })
+        
+        .catch(err => {
+            console.error(err)
+        })
+    },[])
     
 
     return(
@@ -252,20 +278,34 @@ console.log("cal is",ce.N(parse(b)))
                     }}
                 />
                  {
-                    props.name !== "OnePointInteration" &&  
+                    props.name === "Bisection" &&  
                     <div>
                         <input value={test.left}  onChange={inputsHandler} name="left" placeholder={"ขอบเขตซ้าย"}/>
                          <input value={test.right} onChange={inputsHandler} name="right" placeholder={"ขอบเขตขวา"}/>
                     </div>
                 }
                 {
-                    props.name === "OnePointInteration" && 
+                    props.name === "FalsePosition" &&  
+                    <div>
+                        <input value={test.left}  onChange={inputsHandler} name="left" placeholder={"ขอบเขตซ้าย"}/>
+                         <input value={test.right} onChange={inputsHandler} name="right" placeholder={"ขอบเขตขวา"}/>
+                    </div>
+                }
+                {
+                    props.name === "OnePointInteration" &&
+                    <div>
+                        <input value={test.begin}  onChange={inputsHandler} name="begin" placeholder={"ค่าเริ่มต้น"}/>
+                    </div>
+                }
+                {
+                    props.name === "NewtonRaphson" &&
                     <div>
                         <input value={test.begin}  onChange={inputsHandler} name="begin" placeholder={"ค่าเริ่มต้น"}/>
                     </div>
                 }
                 <input type="submit" name="submit"/>
-                <p>{latex.replace(/(\d+)(x)/g, '$1*x').replace(/(\d+)(e)/g, '$1*e')}</p>
+                <p>{latex.replace(/([-+]?[0-9]*\.?[0-9]+)(x)/g, '$1*x').replace(/([-+]?[0-9]*\.?[0-9]+)(e)/g, '$1*e').replace(/\\frac{(.+)}{(.+)}/,'$1/$2').replace(/\\sqrt{([-+]?[0-9]*\.?[0-9]+)}/,'sqrt($1)').replace(/{/g,'(').replace(/}/g,')')}</p>
+                {/* .replace(/([-+]?[0-9]*\.?[0-9]+)(x)/g, '$1*x').replace(/([-+]?[0-9]*\.?[0-9]+)(e)/g, '$1*e').replace(/{\\frac{([-+]?[0-9]*\.?[0-9]+)}{([-+]?[0-9]*\.?[0-9]+)}}/,'($1/$2)') */}
                 <h2>{res}</h2>
                 <div id="test"></div>
                 <LineChart
