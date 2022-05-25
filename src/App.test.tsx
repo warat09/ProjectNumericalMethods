8c6e5c85bda8e52ecc0856bc7f5ae4b8744e7044
warat09 <y-title@hotmail.com>
2022-05-26 03:47:57 +0700

    CD

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -17,16 +17,7 @@ test('screen project',()=>{
 
 it('select chapter1',async ()=>{
   await render(<Chapter1 />)
-  // await fireEvent.change(screen.getByTestId("bileft"),{
-  //   target:{value:"2"}
-  // });
-  // await fireEvent.change(screen.getByTestId("biright"),{
-  //   target:{value:"4"}
-  // });
 
-  // await fireEvent.click(screen.getByText(/submit/i))
-
-  // await expect(screen.getAllByTestId("select-option").length).toBe(4)
   await waitFor(()=>{
     expect(screen.queryByText("Result: 0")).not.toBeInTheDocument()
   })
@@ -34,38 +25,8 @@ it('select chapter1',async ()=>{
   await waitFor(()=>{
      expect(screen.getByTestId("testgg").textContent).toEqual("Result: 1.8988289833068848")
   })
-  // await userEvent.selectOptions(
-  //   // Find the select element, like a real user would.
-  //   await screen.getByRole('combobox'),
-  //   // Find and select the Ireland option, like a real user would.
-  //   await screen.getByRole('option', {name: 'FalsePositionMethod'}),
-  // )
-  // await expect((screen.getByRole('option') as HTMLOptionElement).selected).toBe(true)
-  // await expect((screen.getByRole('option', {name: 'FalsePositionMethod'})as HTMLOptionElement).selected).toBe(true)
-  // await expect((screen.getByRole('option', {name: 'BisectionMethod'})as HTMLOptionElement).selected).toBe(false)
-  // await expect((screen.getByRole('option', {name: 'OnepointInteration'})as HTMLOptionElement).selected).toBe(false)
-  // await expect((screen.getByRole('option', {name: 'NewtonRaphon'})as HTMLOptionElement).selected).toBe(false)
 
-
-
-
-  // await userEvent.selectOptions(screen.getByTestId('selectmethod'), 'FalsePositionMethod');
-  // await expect((screen.getByTestId('selectmethod') as HTMLOptionElement).selected).toBeTruthy();
-
-
-  // await fireEvent.click(screen.getByTestId("selectmethod"),{
-  //   target:{value:"FalsePositionMethod"}
-  // })
-  // let options:any = screen.getAllByTestId('selectoptionmethod')
-  // await expect(options[1].selected).toBeTruthy()
-
-
-
-
-  // await fireEvent.change(screen.getByText("FalsePositionMethod"))
-  // await waitFor(()=>{
-  //    expect(screen.getByText("FalsePositionMethod")).toBeInTheDocument()
-  // })
+  await expect(screen.getByTestId("testgg").textContent).toEqual("Result: 1.8988289833068848")
 
   await waitFor(()=>{
     fireEvent.change(screen.getByTestId("select-problem") as HTMLInputElement,{
@@ -108,15 +69,4 @@ it('select chapter1',async ()=>{
  })
  await expect(screen.getByTestId("testgg").textContent).toEqual("Result: 2.6457513110646933")
 
-
-
-
-
-  // await fireEvent.change(screen.getByTestId("faileft"),{
-  //   target:{value:"0.02"}
-  // });
-  // await fireEvent.change(screen.getByTestId("failright"),{
-  //   target:{value:"0.03"}
-  // });
-  // await fireEvent.click(screen.getByText(/submit/i))
 })
