0c479a653a3e93a44c12aecdb7150b2b2eec3a00
warat09 <y-title@hotmail.com>
2022-05-26 14:13:20 +0700

    [CI]21

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -20,22 +20,22 @@ test('select chapter1',async ()=>{
 
   await waitFor(()=>{
     expect(screen.queryByText("Result: 0")).not.toBeInTheDocument()
-  },{timeout:450})
+  },{timeout:500})
   await waitFor(()=>{
      expect(screen.getByTestId("testgg").textContent).toEqual("Result: 1.8988289833068848")
-  },{timeout:450})
+  },{timeout:500})
 
   await waitFor(()=>{
     fireEvent.change(screen.getByTestId("select-problem") as HTMLInputElement,{
       target:{value:`{"eq":"2x^3-2x-5","left":2,"right":4}`}
     })
- },{timeout:450})
+ },{timeout:500})
  await waitFor(()=>{
    expect((screen.getByTestId("select-problem") as HTMLInputElement).value).toEqual(`{"eq":"2x^3-2x-5","left":2,"right":4}`)
- },{timeout:450})
+ },{timeout:500})
  await waitFor(()=>{
    expect(screen.queryByText("Result: 1.8988289833068848")).not.toBeInTheDocument()
- },{timeout:450})
+ },{timeout:500})
  await expect(screen.getByTestId("testgg").textContent).toEqual("Result: 2.000000476837158")
 
 
@@ -43,13 +43,13 @@ test('select chapter1',async ()=>{
       fireEvent.change(screen.getByTestId("select-option") as HTMLInputElement,{
         target:{value:"FalsePosition"}
       })
-   },{timeout:450})
+   },{timeout:500})
    await waitFor(()=>{
      expect((screen.getByTestId("select-option") as HTMLInputElement).value).toEqual("FalsePosition")
-   },{timeout:450})
+   },{timeout:500})
    await waitFor(()=>{
      expect(screen.queryByText("Result: 2.000000476837158")).not.toBeInTheDocument()
-   },{timeout:450})
+   },{timeout:500})
    await expect(screen.getByTestId("testgg").textContent).toEqual("Result: 0.023255813953488372")
 
 
@@ -57,13 +57,13 @@ test('select chapter1',async ()=>{
     fireEvent.change(screen.getByTestId("select-option") as HTMLInputElement,{
       target:{value:"NewtonRaphson"}
     })
- },{timeout:450})
+ },{timeout:500})
  await waitFor(()=>{
    expect((screen.getByTestId("select-option") as HTMLInputElement).value).toEqual("NewtonRaphson")
- },{timeout:450})
+ },{timeout:500})
  await waitFor(()=>{
    expect(screen.queryByText("Result: 0.023255813953488372")).not.toBeInTheDocument()
- },{timeout:450})
+ },{timeout:500})
  await expect(screen.getByTestId("testgg").textContent).toEqual("Result: 2.6457513110646933")
 
 })
