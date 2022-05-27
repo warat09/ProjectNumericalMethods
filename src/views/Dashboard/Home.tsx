88d4bcf4635c94431cfb09c68c7ab91890090309
warat09 <y-title@hotmail.com>
2022-05-27 22:31:21 +0700

    Homepage

--- src/views/Dashboard/Home.tsx
+++ src/views/Dashboard/Home.tsx
@@ -3,8 +3,23 @@ import React from "react";
 const Home :React.FC = () =>{
   document.title = "Numer Machine"
     return(
-        <div>
-        </div>
+      <div>
+      <header className="bg-white shadow">
+  <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+    <h1 className="text-3xl font-bold text-gray-900">Home</h1>
+  </div>
+</header>
+<main>
+  <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+    {/* <!-- Replace with your content --> */}
+    <div className="px-4 py-6 sm:px-0">
+      <h1>6204062616111 นาย วรัศฐ์ เย็นใจประเสริฐ</h1>
+      <h2>กด Link Procject ข้างบน</h2>
+    </div>
+    {/* <!-- /End replace --> */}
+  </div>
+</main>
+  </div>
     );
 
 };
